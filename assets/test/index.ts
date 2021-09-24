2ffe13066d76f080ec1966ba166f0b1149813d1c
Toto Minai <bufhdy@hotmail.com>
2021-09-24 21:27:18 +0900

    Detect Firefox

--- assets/test/index.ts
+++ assets/test/index.ts
@@ -1,6 +1,14 @@
 import { Tategaki } from "../../src/tategaki"
-// import { detect } from 'detect-browser'
+import { detect } from 'detect-browser'
+
+const browser = detect()
+let isFirefox = browser && browser.name === 'firefox'
 
 let article = document.getElementsByTagName('article')[0]
-let tategaki = new Tategaki(article, true, false)
+let tategaki = new Tategaki(article, true, isFirefox, !isFirefox)
+
 tategaki.parse()
+
+if (isFirefox) {
+    document.body.classList.add('firefox')
+}
