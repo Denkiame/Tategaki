b3fb0d1301e6d5a5e22ecb06a9f21dc2a7f0fad3
Toto Minai <bufhdy@hotmail.com>
2021-09-25 17:47:48 +0900

    Clean for GitHub Page

--- assets/test/index.js
+++
@@ -1,5 +0,0 @@
-import { Tategaki } from "../src/tategaki";
-// import { detect } from 'detect-browser'
-let article = document.getElementsByTagName('article')[0];
-let tategaki = new Tategaki(article, true, false);
-tategaki.parse();
